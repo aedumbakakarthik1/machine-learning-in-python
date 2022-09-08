a99f0eb2d268538394d7fc4a789082f39e3418c7
karthik <karthikaedumbaka1@gmail.com>
2022-09-08 13:16:37 +0530

    lambda Function

---
+++ 11)lambdaFunction.py
@@ -0,0 +1,20 @@
+# LAMBDA Fuction
+"""
+it is alos called as anonymous functions
+
+A function with no name is called as anonymous function or  lambda function
+
+"""
+
+# normal function
+def addition(a,b):
+    return a + b
+a=addition(4,5)
+print(a)
+
+
+# lambda function
+add=lambda a,b : a+b
+print(add(12,4))
+
+# lambda (the inputs we are giving) :  the opertion we need to do
